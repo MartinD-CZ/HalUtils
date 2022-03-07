16c1b4c870c47dfd8cdba030da396472d212fd2b
MartinD-CZ <martin@embedblog.eu>
2022-03-07 13:37:34 +0100

    added hal include

---
+++ Inc/hal_inc.h
@@ -0,0 +1,18 @@
+/* hal_inc.h
+ *
+ * Created on: Mar 7, 2022
+ * Author: Martin Danek, martin@embedblog.eu
+ */
+
+#ifndef HAL_INC_H_
+#define HAL_INC_H_
+
+#include <stdint.h>
+
+#if defined(STM32G0)
+#include "stm32g0xx_hal.h"
+#endif
+
+
+
+#endif /* HAL_INC_H_ */
