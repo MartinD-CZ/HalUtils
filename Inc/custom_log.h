9626efd7fecf31f195b2bba5a66871479097fb3b
MartinD-CZ <martin@embedblog.eu>
2022-03-18 21:46:19 +0100

    basic log implemented

--- Inc/custom_log.h
+++ Inc/custom_log.h
@@ -7,30 +7,55 @@
 #ifndef CUSTOM_LOG_H_
 #define CUSTOM_LOG_H_
 
-enum DebugLevel
+#include <stddef.h>
+
+enum class DebugLevel
 {
-	VERBOSE = 0/*,
-	DEBUG = 1*/,
+	VERBOSE = 0,
+	DBG = 1,
 	INFO = 2,
 	WARNING = 3,
 	ERROR = 4
 };
 
 #ifndef LOG_LEVEL
-#define LOG_LEVEL		0
+#define LOG_LEVEL		0		//sets the minimal level of log, which will be outputted
 #endif
 
 void customLog(DebugLevel debugLevel, const char* msg, ...);
 
+//these two functions must be iomplemented by the user:
+void _log_send(const char* msg, size_t len);
+bool _log_isReady();
+
 #if (LOG_LEVEL < 1)
-#define LOGV(x)				customLog(VERBOSE, x)
+#define LOGV(x)				customLog(DebugLevel::VERBOSE, x)
 #else
 #define LOGV(x)				(void)(x)
 #endif
 
-#define LOGD
-#define LOGI
-#define LOGW
-#define LOGE
+#if (LOG_LEVEL < 2)
+#define LOGD(x)				customLog(DebugLevel::DBG, x)
+#else
+#define LOGD(x)				(void)(x)
+#endif
+
+#if (LOG_LEVEL < 3)
+#define LOGI(x)				customLog(DebugLevel::INFO, x)
+#else
+#define LOGI(x)				(void)(x)
+#endif
+
+#if (LOG_LEVEL < 4)
+#define LOGW(x)				customLog(DebugLevel::WARNING, x)
+#else
+#define LOGW(x)				(void)(x)
+#endif
+
+#if (LOG_LEVEL < 5)
+#define LOGE(x)				customLog(DebugLevel::ERROR, x)
+#else
+#define LOGE(x)				(void)(x)
+#endif
 
 #endif /* CUSTOM_LOG_H_ */
--- Src/custom_log.cpp
+++ Src/custom_log.cpp
@@ -6,9 +6,42 @@
 
 #include "custom_log.h"
 
+#include "hal_inc.h"
+#include "custom_assert.h"
+
+#include <stdio.h>
+
+#ifndef LOG_BUF_SIZE
+#define LOG_BUF_SIZE		128
+#endif
+
+#ifndef LOG_TIMEOUT
+#define LOG_TIMEOUT			250
+#endif
+
+static const char logChr[] = {'V', 'D', 'I', 'W', 'E'};
+static char buf[LOG_BUF_SIZE];
+
 void customLog(DebugLevel debugLevel, const char* msg, ...)
 {
+	auto start = HAL_GetTick();
+	while (!_log_isReady())
+		ASSERT_WITH_RETURN(HAL_GetTick() - start < LOG_TIMEOUT);
 
+	auto len = snprintf(buf, LOG_BUF_SIZE, "%lu %c: %s", start, logChr[(size_t)debugLevel], msg);
+
+	_log_send(buf, len);
+}
+
+__WEAK void _log_send(const char* msg, size_t len)
+{
+	__asm("bkpt 0");
+}
+
+__WEAK bool _log_isReady()
+{
+	__asm("bkpt 0");
+	return false;
 }
 
 
